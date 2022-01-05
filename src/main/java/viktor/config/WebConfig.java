10803145f9b3e0c43c48e50ef1bb565f9cc2a9d5
Lipin Viktor <lipivv1986@gmail.com>
2022-01-05 22:44:36 +1000

    add new

--- src/main/java/viktor/config/WebConfig.java
+++ src/main/java/viktor/config/WebConfig.java
@@ -5,12 +5,14 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -25,6 +27,8 @@ import java.util.Properties;
 @Configuration
 @EnableWebMvc
 @ComponentScan("viktor")
+@PropertySource("classpath:db.properties")
+@EnableTransactionManagement
 public class WebConfig implements WebMvcConfigurer {
     private final ApplicationContext applicationContext;
     private final Environment env;
